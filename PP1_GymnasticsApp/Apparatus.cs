8c3010ab63d5842ef53e16f4b06ece2ab445595d
Evans WeCanCodeIT <Evans WeCanCodeIT>
2016-03-18 23:17:34 -0400

    PP1 initial class structure

---
+++ PP1_GymnasticsApp/Apparatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP1_GymnasticsApp
+{
+    class Apparatus
+    {
+        //Fields
+        private double score;
+        private int place;
+        private bool tie;
+
+        //Properties
+        public double Score { get; set; }
+        public int Place { get; set; }
+        public bool Tie { get; set; }
+
+        //Constructor
+        public Apparatus()
+        {
+
+        }
+
+    }
+}
---
+++ PP1_GymnasticsApp/Meet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP1_GymnasticsApp
+{
+    class Meet
+    {
+        //Fields
+        private string name;
+        private string host;
+        private string venue;
+        private string city;
+        private DateTime eventDate;
+        private DateTime eventTime;
+        private string level;
+        private string group;
+
+        //Constructor
+        public Meet()
+        {
+
+        }
+    }
+}
